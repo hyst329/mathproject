c5b2c609927d24ddfa92b974f00c2b5e6a6f7214
dragon116 <rturbogda@yandex.ru>
2015-07-06 11:54:42 +0300

    update

--- interpreter/AST.h
+++ interpreter/AST.h
@@ -4,7 +4,9 @@
 
 #ifndef MATHPROJECT_AST_H
 #define MATHPROJECT_AST_H
-
+#include <string>
+#include <vector>
+using namespace std;
 namespace Interpreter {
     struct AST {
         virtual void exec() = 0;
@@ -12,7 +14,18 @@ namespace Interpreter {
 
     struct RealAST : public AST {
         double value;
+        RealAST(double Value):value(Value){};
     };
+    struct GlobalAST: public AST {
+    private:
+        string Operation;
+        vector <AST> *vast;
+    public:
+        GlobalAST(string operation, vector <AST> *vAst)
+        : Operation(operation), vast(vAst) {}
+    };
+
+
 }
 
 #endif //MATHPROJECT_AST_H
