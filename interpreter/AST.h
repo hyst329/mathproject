f336da6badc490199d869044b8bdeb63a8e63247
dragon116 <rturbogda@yandex.ru>
2015-07-06 13:58:25 +0300

    update

--- interpreter/AST.h
+++ interpreter/AST.h
@@ -8,6 +8,7 @@
 #include <vector>
 using namespace std;
 namespace Interpreter {
+
     struct AST {
         virtual void exec() = 0;
     };
@@ -22,9 +23,36 @@ namespace Interpreter {
         vector <AST> *vast;
     public:
         GlobalAST(string operation, vector <AST> *vAst)
-        : Operation(operation), vast(vAst) {}
+            :Operation(operation), vast(vAst) {}
     };
+    class BinaryAST: public AST {
+        string Operation;
+        AST *left,*right;
+    public:
+        BinaryAST(string operation,AST *Left,AST *Right)
+                :Operation(operation),left(Left),right(Right){};
+    };
+    class UnaryAST: public AST {
+        string Operation;
+        AST *ast;
+    public:
+        UnaryAST(string operation,AST *Ast)
+                :Operation(operation),ast(Ast){};
+    };
+    class VariableAST : public AST {
+        string Name;
 
+    public:
+        VariableAST(string name)
+                : Name(name) {}
+    };
+    class ConditionalAST:public AST {
+        string Operation;
+        AST *IfAST,*ThenAST,*ElseAST;
+    public:
+        ConditionalAST(string operation,AST *ifAST,AST *thenAST,AST *elseAST)
+                :Operation(operation),IfAST(ifAST),ThenAST(thenAST),ElseAST(elseAST){};
+    };
 
 }
 
