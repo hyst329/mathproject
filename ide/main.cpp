4083f9acfa2e9a34103a43fba932ee9143a7d880
hyst329 <trolleybus.1329@gmail.com>
2015-07-13 11:52:26 +0300

    temporary branch for working with gcc 4.9.2

--- ide/main.cpp
+++ ide/main.cpp
@@ -3,12 +3,12 @@
 int main(int argc, char **argv)
 {
     // TODO(hyst329) : Rebuild gtkmm and check with rebuilt libraries (undefined references)
-//    Gtk::Main app(argc, argv);
-//    Glib::RefPtr<Gtk::Builder> builder = Gtk::Builder::create_from_file(Glib::ustring("userInterface.glade"));
-//    Gtk::Window *w;
-//    builder->get_widget("window_main", w);
-//    w->show_all_children();
-//    app.run(*w);
+    Gtk::Main app(argc, argv);
+    Glib::RefPtr<Gtk::Builder> builder = Gtk::Builder::create_from_file(Glib::ustring("userInterface.glade"));
+    Gtk::Window *w;
+    builder->get_widget("window_main", w);
+    w->show_all_children();
+    app.run(*w);
     return 0;
 }
 
